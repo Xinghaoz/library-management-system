5669ed9f3b0f5cda5b1bff4d32d9b5767b285669
Xinghaoz <zhou.xinghao.1991@gmail.com>
2017-04-06 23:04:33 -0700

    Add project

---
+++ library-management-system/src/main/java/yanghongquan/library_management_system/App.java
@@ -0,0 +1,13 @@
+package yanghongquan.library_management_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
